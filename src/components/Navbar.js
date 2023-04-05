78a4470f142dc5ad2f2ff749332b771a14f1e920
maina lawrence <mainalawrence32@gmail.com>
2023-04-05 17:38:11 +0300

    susan portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -82,19 +82,11 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
+
 
             <Nav.Item>
               <Nav.Link
-                href="https://"
+                href="https://susanbella.wordpress.com/2022/12/20/types-of-skins-for-different-people/"
                 target="_blank"
                 rel="noreferrer"
               >
