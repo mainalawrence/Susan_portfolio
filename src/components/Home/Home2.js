5128876b8df34867552bc99c02624d92a889e631
maina lawrence <mainalawrence32@gmail.com>
2023-04-06 09:49:54 +0300

    susan porfolio

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -79,7 +79,7 @@ function Home2() {
               </li> */}
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/mainalawrence32"
+                  href="https://www.instagram.com/suzanmuriithi/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
